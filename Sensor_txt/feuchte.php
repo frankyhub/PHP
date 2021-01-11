8f3defad0cb167cc13ec2d25a9dbdd8e3407f676
frankyhub <61152841+frankyhub@users.noreply.github.com>
2021-01-11 10:07:20 +0100

    Add files via upload

---
+++ Sensor_txt/feuchte.php
@@ -0,0 +1,14 @@
+<?php
+
+$feuchte = '67'; // Declares the request from index.html as a variable
+$textfile = "feuchte.txt"; // Declares the name and location of the .txt file
+
+$fileLocation = "$textfile";
+$fh = fopen($fileLocation, 'w   ') or die("Something went wrong!"); // Opens up the .txt file for writing and replaces any previous content
+//$stringToWrite = "$onoroff"; // Write either 1 or 0 depending on request from index.html
+$stringToWrite = $feuchte;
+fwrite($fh, $stringToWrite); // Writes it to the .txt file
+fclose($fh);
+
+//header("Location: led.html"); // Return to frontend (index.html)
+?>
